d47035adcc19faaee48d9c892c7fd09dc5dcd6d3
BlockchainFor <42303494+BlockchainFor@users.noreply.github.com>
2019-01-21 15:28:38 +0300

    Update evos-update-testnet.sh

--- evos-update-testnet.sh
+++ evos-update-testnet.sh
@@ -2,7 +2,7 @@
 
 evos-cli stop
 sleep 2
-rm -rf .evos/testnet4/mncache.dat .evos/testnet4/mnpayments.dat .evos/testnet4/peers.dat
+rm -rf .evos/mncache.dat .evos/mnpayments.dat .evos/peers.dat
 rm -rf /usr/local/bin/evos*
 rm -rf evos-cli_ubuntu-16.04-x64.tar.bz2
 wget https://github.com/EVOS-DEV/evos-core/releases/download/1.1.2/evos-1.1.2-ubuntu-daemon.tgz
@@ -13,5 +13,5 @@ cp -p -r evosd /usr/local/bin
 cp -p -r evos-cli /usr/local/bin
 rm -rf evos-1.1.1-ubuntu-daemon.tgz
 rm -rf evos-1.1.2-ubuntu-daemon.tgz
-evosd -daemon -testnet -reindex
+evosd -daemon
 evos-cli --version
