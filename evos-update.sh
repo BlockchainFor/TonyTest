3ff738273ddc2372c5a51bff6ebdfef9bc2bba89
BlockchainFor <42303494+BlockchainFor@users.noreply.github.com>
2019-01-26 20:57:54 +0300

    Update evos-update.sh

--- evos-update.sh
+++ evos-update.sh
@@ -5,13 +5,13 @@ sleep 2
 rm -rf .evos/mncache.dat .evos/mnpayments.dat .evos/peers.dat
 rm -rf /usr/local/bin/evos*
 rm -rf evos-cli_ubuntu-16.04-x64.tar.bz2
-wget https://github.com/EVOS-DEV/evos-core/releases/download/1.1.2/evos-1.1.2-ubuntu-daemon.tgz
-tar -xvf evos-1.1.2-ubuntu-daemon.tgz
+wget https://github.com/EVOS-DEV/evos-core/releases/download/1.1.3/evos-1.1.3-ubuntu-daemon.tgz
+tar -xvf evos-1.1.3-ubuntu-daemon.tgz
 sudo chmod -R 755 evos-cli
 sudo chmod -R 755 evosd
 cp -p -r evosd /usr/local/bin
 cp -p -r evos-cli /usr/local/bin
-rm -rf evos-1.1.1-ubuntu-daemon.tgz
-rm -rf evos-1.1.2-ubuntu-daemon.tgz
+rm -rf *ubuntu-daemon.tgz
+rm -rf *ubuntu-daemon.tgz
 evosd -daemon
 evos-cli --version
